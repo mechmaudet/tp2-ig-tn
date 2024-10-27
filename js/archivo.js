f6aad8ea3f565e59ee518d093ce226668be4809d
mechmaudet <mechiolivera@gmail.com>
2024-10-27 20:05:29 -0300

    Cambio de logo + agregado de páginas de direccion de arte + agregado de obras en acrilico + interacción con subsecciones

--- js/archivo.js
+++ js/archivo.js
@@ -1,4 +1,8 @@
 let btnHome = document.querySelector("#home");
+let btnHome2 = document.querySelector("#home2");
 
 btnHome.addEventListener("hover", () => btnHome.style.cursor = "url(../imgs/cursor2.png), auto");
 btnHome.addEventListener("click", () => window.location.href = "index.html");
+
+btnHome2.addEventListener("hover", () => btnHome2.style.cursor = "url(../imgs/cursor2.png), auto");
+btnHome2.addEventListener("click", () => window.location.href = "../index.html");
